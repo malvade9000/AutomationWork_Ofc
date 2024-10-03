dfef11aa2298438451926191b74fb5ca599fa3d3
Aniket Ramesh Malvade <malvade9000@gmail.com>
2024-10-03 18:10:36 +0530

    03 10 2024

---
+++ src/Sept/ex_04092024/Lab003.java
@@ -0,0 +1,13 @@
+package Sept.ex_04092024;
+
+import java.sql.SQLOutput;
+
+public class Lab003 {
+    public static void main (String[] args){
+
+
+        System.out.println("Hi Aniket");
+
+
+    }
+}
---
+++ src/Sept/ex_06092024/Lab004.java
@@ -0,0 +1,7 @@
+package Sept.ex_06092024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Good morning Aniket");
+    }
+}
